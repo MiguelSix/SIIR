fb830c42c7a649d0f4bf7d159f34b23bfae033d8
CarlosRT1411 <l20140951@queretaro.tecnm.mx>
2024-09-11 20:09:52 -0600

    StudentDashboard view

--- SIIR/Controllers/HomeController.cs
+++ SIIR/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace SIIR.Controllers
             return View();
         }
 
+        public IActionResult StudentDashboard()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
